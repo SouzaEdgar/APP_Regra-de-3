5415377d82a3e5d99242882448326c9d1c02657b
SouzaEdgar <edgarss2013@gmail.com>
2026-08-24 23:06:42 -0300

    chore: atualiza versão do gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "com.sheepblue.regrade3"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(37)
     }
 
     defaultConfig {
