18bf3e4b817d781eaa4d81b0108e9f7ddbac665e
SouzaEdgar <edgarss2013@gmail.com>
2026-08-27 23:19:30 -0300

    feat: adiciona delay no efeito do botão calcular

--- app/src/main/java/com/sheepblue/regrade3/ui/calculator/components/CalculateButton.kt
+++ app/src/main/java/com/sheepblue/regrade3/ui/calculator/components/CalculateButton.kt
@@ -1,14 +1,7 @@
 package com.sheepblue.regrade3.ui.calculator.components
 
 import android.content.res.Configuration
-import androidx.compose.animation.core.AnimationSpec
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.repeatable
-import androidx.compose.animation.core.snap
-import androidx.compose.animation.core.spring
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -20,36 +13,46 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sheepblue.regrade3.ui.theme.RegraDe3Theme
+import kotlinx.coroutines.delay
 
 @Composable
 fun CalculateButton(
     onClick: () -> Unit
 ) {
-    val pressButton = remember { MutableInteractionSource() }
+    var clicked by remember { mutableStateOf(false) }
 
-    val pressed by pressButton.collectIsPressedAsState()
-
-    val cornerRadius by animateDpAsState(
-        targetValue = if (pressed) 4.dp else 24.dp
+    val shapeRadius by animateDpAsState(
+        targetValue = if (clicked) 4.dp else 24.dp
     )
-    val shape = RoundedCornerShape(size = cornerRadius)
 
     Row(
         modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
-    ){
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
         OutlinedButton(
-            onClick = { onClick() },
-            interactionSource = pressButton,
-            shape = shape
+            onClick = {
+                clicked = true
+                onClick()
+            },
+            shape = RoundedCornerShape(size = shapeRadius)
         ) {
             Text("CALCULAR")
+        }
+    }
+
+    LaunchedEffect(clicked) {
+        if (clicked) {
+            delay(timeMillis = 300)
+            clicked = false
         }
     }
 }
