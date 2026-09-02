fc36851d27ef6948eef2b600731820e052f01fa2
SouzaEdgar <edgarss2013@gmail.com>
2026-09-02 13:04:57 -0300

    feat: define CalculatorScreen() como tela inicial

--- app/src/main/java/com/sheepblue/regrade3/MainActivity.kt
+++ app/src/main/java/com/sheepblue/regrade3/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.sheepblue.regrade3.ui.calculator.CalculatorScreen
 import com.sheepblue.regrade3.ui.theme.RegraDe3Theme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +20,8 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RegraDe3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CalculatorScreen()
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RegraDe3Theme {
-        Greeting("Android")
     }
 }
