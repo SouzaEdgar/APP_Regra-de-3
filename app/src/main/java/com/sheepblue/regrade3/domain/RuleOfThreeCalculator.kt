356abfb2871e1db93e0f51ff35445cbf4c7d5c1d
SouzaEdgar <edgarss2013@gmail.com>
2026-09-02 18:08:09 -0300

    refactor: move logica de resultado do calculo para RuleOfThreeCalculator

--- app/src/main/java/com/sheepblue/regrade3/domain/RuleOfThreeCalculator.kt
+++ app/src/main/java/com/sheepblue/regrade3/domain/RuleOfThreeCalculator.kt
@@ -2,12 +2,35 @@ package com.sheepblue.regrade3.domain
 
 import com.sheepblue.regrade3.domain.enums.CalculationType
 import com.sheepblue.regrade3.domain.model.RuleOfThree
+import com.sheepblue.regrade3.domain.model.RuleOfThreeResult
 
 class RuleOfThreeCalculator {
-    fun calculate(ruleOfThree: RuleOfThree): Double {
-        return when(ruleOfThree.type) {
-            CalculationType.DIRECT -> calculateDirect(ruleOfThree)
-            CalculationType.INVERSE -> calculateInverse(ruleOfThree)
+    fun calculate(rule: RuleOfThree): RuleOfThreeResult {
+        var hasResult = true
+
+        if (rule.type == CalculationType.DIRECT) {
+            if (rule.valueA == 0.0) hasResult = false
+        } else {
+            if (rule.valueC == 0.0) hasResult = false
+        }
+
+        when(rule.type) {
+            CalculationType.DIRECT -> return RuleOfThreeResult(
+                result = calculateDirect(rule),
+                formulaNumerator = "B * C",
+                formulaDenominator = "A",
+                expressionNumerator = "${rule.valueB} * ${rule.valueC}",
+                expressionDenominator = "${rule.valueA}",
+                hasResult = hasResult
+            )
+            CalculationType.INVERSE -> return RuleOfThreeResult(
+                result = calculateInverse(rule),
+                formulaNumerator = "A * B",
+                formulaDenominator = "C",
+                expressionNumerator = "${rule.valueA} * ${rule.valueB}",
+                expressionDenominator = "${rule.valueC}",
+                hasResult = hasResult
+            )
         }
     }
 
