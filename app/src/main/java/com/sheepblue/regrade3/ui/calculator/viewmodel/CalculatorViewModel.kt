ba71cbb448ca89bfd0feedf95e348a5d4a6a7c80
SouzaEdgar <edgarss2013@gmail.com>
2026-09-03 01:54:41 -0300

    fix: corrige criação da ViewModel pelo Hilt

--- app/src/main/java/com/sheepblue/regrade3/ui/calculator/viewmodel/CalculatorViewModel.kt
+++ app/src/main/java/com/sheepblue/regrade3/ui/calculator/viewmodel/CalculatorViewModel.kt
@@ -12,9 +12,10 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import javax.inject.Inject
 
 @HiltViewModel
-class CalculatorViewModel: ViewModel() {
+class CalculatorViewModel @Inject constructor(): ViewModel() {
     private val _uiState = MutableStateFlow(CalculatorUiState())
 
     val uiState = _uiState.asStateFlow()
