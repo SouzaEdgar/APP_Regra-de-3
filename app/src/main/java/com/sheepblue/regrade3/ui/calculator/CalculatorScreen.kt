921ae7979a54df506a49faaba3d8f8dba762ead7
SouzaEdgar <edgarss2013@gmail.com>
2026-09-02 23:28:11 -0300

    refactor: atualiza CalculatorScreen para consumir CalculatorViewModel

--- app/src/main/java/com/sheepblue/regrade3/ui/calculator/CalculatorScreen.kt
+++ app/src/main/java/com/sheepblue/regrade3/ui/calculator/CalculatorScreen.kt
@@ -7,8 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableDoubleStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,16 +17,17 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
 import com.sheepblue.regrade3.domain.RuleOfThreeCalculator
 import com.sheepblue.regrade3.domain.enums.CalculationType
 import com.sheepblue.regrade3.domain.enums.InputError
 import com.sheepblue.regrade3.domain.model.CalculationResult
 import com.sheepblue.regrade3.domain.model.RuleOfThree
-import com.sheepblue.regrade3.domain.model.RuleOfThreeResult
 import com.sheepblue.regrade3.ui.calculator.components.CalculateButton
 import com.sheepblue.regrade3.ui.calculator.components.CalculationTypeSelector
 import com.sheepblue.regrade3.ui.calculator.components.CalculatorTable
 import com.sheepblue.regrade3.ui.calculator.components.ResultCard
+import com.sheepblue.regrade3.ui.calculator.viewmodel.CalculatorViewModel
 import com.sheepblue.regrade3.utils.isValidNumber
 import com.sheepblue.regrade3.utils.validateInputs
 
@@ -49,18 +50,22 @@ import com.sheepblue.regrade3.utils.validateInputs
  */
 
 @Composable
-fun CalculatorScreen() {
-    var numA by rememberSaveable { mutableStateOf("") }
-    var numB by rememberSaveable { mutableStateOf("") }
-    var numC by rememberSaveable { mutableStateOf("") }
+fun CalculatorScreen(
+    viewModel: CalculatorViewModel
+) {
+    val uiState by viewModel.uiState.collectAsState()
 
-    var selectedType by remember { mutableStateOf(CalculationType.DIRECT) }
+    //var numA by rememberSaveable { mutableStateOf("") }
+    //var numB by rememberSaveable { mutableStateOf("") }
+    //var numC by rememberSaveable { mutableStateOf("") }
+
+    //var selectedType by remember { mutableStateOf(CalculationType.DIRECT) }
 
     var showCard by remember { mutableStateOf(false) }
 
-    var calculationResult by remember { mutableStateOf<CalculationResult?>(null) }
+    //var calculationResult by remember { mutableStateOf<CalculationResult?>(null) }
 
-    val wrongInput = remember { mutableStateListOf<InputError>() }
+    //val wrongInput = remember { mutableStateListOf<InputError>() }
 
     Column(
         modifier = Modifier.fillMaxSize(),
@@ -68,78 +73,48 @@ fun CalculatorScreen() {
     ) {
         Column {
             CalculatorTable(
-                numA = numA,
-                numB = numB,
-                numC = numC,
+                numA = uiState.numA,
+                numB = uiState.numB,
+                numC = uiState.numC,
                 onNumAChange = {
                     showCard = false
-                    wrongInput.remove(InputError.VALUE_A)
-                    if (isValidNumber(it)) numA = it
+                    if (isValidNumber(it)) viewModel.onNumAChange(it)
                },
                 onNumBChange = {
                     showCard = false
-                    wrongInput.remove(InputError.VALUE_B)
-                    if (isValidNumber(it)) numB = it
+                    if (isValidNumber(it)) viewModel.onNumBChange(it)
                 },
                 onNumCChange = {
                     showCard = false
-                    wrongInput.remove(InputError.VALUE_C)
-                    if (isValidNumber(it)) numC = it
+                    if (isValidNumber(it)) viewModel.onNumCChange(it)
                 },
-                wrongInput = wrongInput
+                wrongInput = uiState.wrongInput
             )
             Spacer(modifier = Modifier.height(3.dp))
             CalculationTypeSelector(
-                selectedType = selectedType,
+                selectedType = uiState.selectedType,
                 options = CalculationType.entries,
                 onClick = {
                     showCard = false
-                    selectedType = it
-                    wrongInput.clear()
-                    calculationResult = null
+                    viewModel.onTypeSelected()
                 }
             )
         }
         // TODO: mover controle de estado e calculo para ViewModel
         CalculateButton {
-            wrongInput.clear()
-            val validate = validateInputs(numA, numB, numC)
-
-            if (validate.isEmpty()) {
-                calculationResult = RuleOfThreeCalculator().calculate(
-                    RuleOfThree(
-                        valueA = numA.toDouble(),
-                        valueB = numB.toDouble(),
-                        valueC = numC.toDouble(),
-                        type = selectedType
-                    )
-                )
-
-                when(val result = calculationResult) {
-                    is CalculationResult.Success -> {
-                        showCard = true
-                    }
-
-                    is CalculationResult.Error -> {
-                        wrongInput.addAll(result.errors)
-                    }
-
-                    else -> {}
-                }
-            } else {
-                wrongInput.addAll(validate)
-            }
+            viewModel.onCalculateClick()
         }
 
-        AnimatedVisibility(visible = showCard) {
-            when(val result = calculationResult) {
-                is CalculationResult.Success -> {
+        when(val result = uiState.calculationResult) {
+            is CalculationResult.Success -> {
+                AnimatedVisibility(visible = true) {
                     ResultCard(
                         result = result.result
                     )
                 }
-                else -> {}
             }
+            is CalculationResult.Error -> {}
+            else -> {}
         }
     }
 }
@@ -147,5 +122,7 @@ fun CalculatorScreen() {
 @Composable
 @Preview(showBackground = true)
 fun CalculatorScreenPreview() {
-    CalculatorScreen()
+    CalculatorScreen(
+        viewModel = CalculatorViewModel()
+    )
 }
